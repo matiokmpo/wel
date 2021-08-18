e0b572fe1217de938a78306ee31b1787e60f8468
mati <matiokmpo94@gmail.com>
2021-08-18 10:58:45 -0400

    ultimos cambios

--- assets/js/metodos.js
+++ assets/js/metodos.js
@@ -1,6 +1,30 @@
 
-/*ACCEDER AL DOCUMENTO */
-/*var one1 = document.getElementById("switch");
-    console.log("Hola");
-    alert(one1);*/
-    //one1.innerHTML="Hola";
+/*VARAIBLES*/
+let p_profesional = document.getElementById('p_profesional');
+let p_emprendedor = document.getElementById('p_emprendedor');
+let p_empresa = document.getElementById('p_empresa');
+let btn_action=document.getElementById('switch');
+
+
+btn_action.addEventListener('click',function(){
+  btn_action.classList.toggle('active');
+  if(btn_action.classList.contains('active')){
+    p_profesional.innerHTML="Gs. 440.000/mes";
+    p_emprendedor.innerHTML="Gs. 550.000/mes";
+    p_empresa.innerHTML="A convenir";
+  }
+  else{
+    p_profesional.innerHTML="Gs. 400.000/anual";
+    p_emprendedor.innerHTML="Gs. 450.000/anual";
+    p_empresa.innerHTML="A convenir";
+  }
+})
+
+
+addEventListener('DOMContentLoaded',() => {
+  let btn_menu = document.getElementById('btn-menu');
+  btn_menu.addEventListener('click',()=>{
+    let nav_menu=document.getElementById('nav_menu');
+    nav_menu.classList.toggle('show');
+  })
+})
